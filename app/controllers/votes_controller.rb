1842faf07d17c3820e1bace67fe63d7bc755829c
Lisa Plesko <lisa.plesko@bookbub.com>
2017-03-07 16:10:06 -0500

    controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,13 +2,8 @@ class VotesController < ApplicationController
   skip_before_filter  :verify_authenticity_token
 
   def create
-    p params[:actions]
-    p vote_params
+    p params[:payload]
 
     render json: { response_type: "ephemeral", replace_original: true, text: "Thank you for voting." }, status: :ok
-  end
-
-  def vote_params
-    params.require(:actions)
   end
 end
