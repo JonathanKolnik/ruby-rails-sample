eaa8d0fc76e5fd47bc1e479460d3e320242d9602
Lisa Plesko <lisa.plesko@bookbub.com>
2017-03-07 15:35:49 -0500

    Update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get '/pull' => 'welcome#pull_from_gmail'
-  resources :votes, only: [:create]
+  post '/votes' => 'votes#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
