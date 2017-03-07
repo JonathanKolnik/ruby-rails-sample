60c4cf3f70c7829e63ae4ce9c7a422fc431627c8
Lisa Plesko <lisa.plesko@bookbub.com>
2017-03-07 15:56:54 -0500

    controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,12 @@
 class VotesController < ApplicationController
   def create
+    p params[:actions]
+    p vote_params
 
-    p params
+    render json: { response_type: "ephemeral", replace_original: true, text: "Thank you for voting." }, status: :ok
+  end
+
+  def vote_params
+    params.require(:actions)
   end
 end
